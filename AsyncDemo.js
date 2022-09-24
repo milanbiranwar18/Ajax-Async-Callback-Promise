17313db227f790625f7a2159d961161f985dba04
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-24 14:45:50 +0530

    Program for demonstrating async nature of javascript

--- AsyncDemo.js
+++ AsyncDemo.js
@@ -1,1 +1,14 @@
-console.log("Wellcome to the Ajax–Async, Callback & Promise");
+function showTime()
+{
+    const date=new Date();
+    return date.getHours()+" Hrs: "+date.getMinutes()+" Mins: "+date.getSeconds()+" secs: ";
+}
+
+function showSessionExpire()
+{
+    console.log("Activity B: your session expired at "+showTime());
+  
+}
+ console.log("Activity -A :Triggering Activity - B at " +showTime());
+ setTimeout(showSessionExpire,5000);
+ console.log("Activity -A :Triggering Activity - B " +showTime());
